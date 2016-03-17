d12ee31e5528cfb4b717d222a265e336639e75d8
calebgregory <calebgregory@gmail.com>
2016-03-17 11:11:23 -0500

    Adding index.js and npm

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Service } = require('node-windows');
+
+const svc = new Service({
+  name: 'Herrrrrro wollllllld',
+  description: 'A test windows service to see if we can get this puppy working',
+  script: 'C:\\Users\\calebgregory\\code\\node-windows-service\\index.js'
+});
+
+svc.on('install', () => svc.start());
+
+svc.install();
